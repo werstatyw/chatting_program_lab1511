216d4e1a1c5c50101aa8402efe58e1f78609c16e
hallgató <hallgat@LABOR115-02>
2017-11-15 11:57:15 +0100

    Add project files.

---
+++ chatting_program_lab1511/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chatting_program_lab1511
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
